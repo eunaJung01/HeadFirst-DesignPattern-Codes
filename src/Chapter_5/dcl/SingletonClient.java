34cf50f5412c4296876315045c00e06eceaaf5f8
euna <christinejung10@daum.net>
2023-01-05 23:54:33 +0900

    add(Chapter_5): Chapter 5. 싱글턴 패턴 (코드)

---
+++ src/Chapter_5/dcl/SingletonClient.java
@@ -0,0 +1,10 @@
+package Chapter_5.dcl;
+
+public class SingletonClient {
+
+	public static void main(String[] args) {
+		Singleton singleton = Singleton.getInstance();
+		System.out.println(singleton.getDescription());
+	}
+
+}
---
+++ src/Chapter_5/threadsafe/SingletonClient.java
@@ -0,0 +1,10 @@
+package Chapter_5.threadsafe;
+
+public class SingletonClient {
+
+	public static void main(String[] args) {
+		Singleton singleton = Singleton.getInstance();
+		System.out.println(singleton.getDescription());
+	}
+
+}
