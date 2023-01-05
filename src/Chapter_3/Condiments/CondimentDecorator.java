d974dca32499e3c0ace8a51189fbfa88e5c055a3
euna <christinejung10@daum.net>
2023-01-05 23:00:28 +0900

    feat(Chapter_3): 데코레이터에 대한 의문 해결 코드 추가

--- src/Chapter_3/Condiments/CondimentDecorator.java
+++ src/Chapter_3/Condiments/CondimentDecorator.java
@@ -13,4 +13,8 @@ public abstract class CondimentDecorator extends Beverage {
         return beverage.getSize();
     }
 
+    public void setSize(Size size) {
+        beverage.setSize(size);
+    }
+
 }
