40df57d72cdc3eac94c1bcecec270f1edd16dc0d
euna <christinejung10@daum.net>
2023-01-13 22:18:40 +0900

    add(Chapter_8): Chapter 8. 템플릿 메소드 패턴 (코드)

---
+++ src/Chapter_8/barista/beverages/CaffeineBeverage.java
@@ -0,0 +1,24 @@
+package Chapter_8.barista.beverages;
+
+public abstract class CaffeineBeverage {
+
+    public final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+
+    abstract void brew();
+
+    abstract void addCondiments();
+
+    void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+
+}
---
+++ src/Chapter_8/barista/hooks/TeaWithHook.java
@@ -0,0 +1,39 @@
+package Chapter_8.barista.hooks;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TeaWithHook extends CaffeineBeverageWithHook {
+
+    public void brew() {
+        System.out.println("Steeping the tea");
+    }
+
+    public void addCondiments() {
+        System.out.println("Adding Lemon");
+    }
+
+    public boolean customerWantsCondiments() {
+        String answer = getUserInput();
+        return answer.toLowerCase().startsWith("y");
+    }
+
+    private String getUserInput() {
+        String answer = null;
+
+        System.out.print("Would you like lemon with your tea (y/n)? ");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            answer = in.readLine();
+        } catch (IOException ioe) {
+            System.err.println("IO error trying to read your answer");
+        }
+
+        if (answer == null) {
+            return "no";
+        }
+        return answer;
+    }
+
+}
